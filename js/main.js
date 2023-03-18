b22a50949211f5606cc9a75909dddd0c4e14db87
Dat <tuyendat2421@gmail.com>
2023-03-18 14:16:01 +0700

    test

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// SLIDER
+const sliderImg = document.querySelectorAll(".slider-img");
+const imgWidth = document.querySelector(".slider-img").clientWidth;
+const slider = document.querySelector(".slider-inner");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+console.log(nextBtn)
+
+let current = 1;
+slider.style.transform = `translateX(${-imgWidth}px)`;
+
+setInterval(() => {
+    if (current >= sliderImg.length - 1) return;
+    slider.style.transition = '.7s ease-in-out transform';
+    current++;
+    slider.style.transform = `translateX(${-imgWidth * current}px)`;
+    
+},5000)
+
+prevBtn.addEventListener('click', () => {
+    if (current <= 0) return;
+    slider.style.transition = '.7s ease-in-out transform';
+    current--;
+    slider.style.transform = `translateX(${-imgWidth * current}px)`;
+
+})
+
+nextBtn.addEventListener('click', () => {
+    if (current >= sliderImg.length - 1) return;
+    slider.style.transition = '.7s ease-in-out transform';
+    current++;
+    slider.style.transform = `translateX(${-imgWidth * current}px)`;
+})
+
+slider.addEventListener('transitionend', () => {
+    if (sliderImg[current].classList.contains('first-img')) {
+        slider.style.transition = 'none';
+        current = sliderImg.length - 2;
+        slider.style.transform = `translateX(${-imgWidth * current}px)`;
+        console.log('true-frist');
+
+    }
+
+    if (sliderImg[current].classList.contains('last-img')) {
+        slider.style.transition = 'none';
+        
+        current = sliderImg.length - current;
+        slider.style.transform = `translateX(${-imgWidth * current}px)`;
+        console.log('true-last');
+    }
+})
+// SLIDER
+
+
